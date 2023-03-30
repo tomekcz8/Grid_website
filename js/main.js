20e4f25eac3dc323f2c99cc5fa0e6ca403b54247
tomekcz8 <39481830+tomekcz8@users.noreply.github.com>
2023-03-30 19:38:01 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+const allNavItems = document.querySelectorAll('.nav__link')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navBtn.classList.remove('is-active')
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerHTML = year
+}
+handleCurrentYear()
